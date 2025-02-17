c31cbb30013e94f4cab3c621e9ea4d9eea6a4f98
Gabriel Dornelas <159338037+gddornelas@users.noreply.github.com>
2025-02-17 08:48:31 -0300

    Add files via upload

---
+++ projeto20250217/src/main/java/com/mycompany/projeto20250217/Projeto20250217.java
@@ -0,0 +1,105 @@
+
+package com.mycompany.projeto20250217;
+import java.util.Scanner;
+/**
+ *
+ * @author alunolab03
+ */
+public class Projeto20250217 { 
+     public static void main(String[] args) {
+         
+         
+        Scanner scan = new Scanner(System.in);
+        
+        int operacao = 0;
+        
+        
+        /**while(operacao != 4){
+            System.out.println("-----------------------MENU--------------------------");
+            System.out.println("\t\t[0] - soma(+)");
+            System.out.println("\t\t[1] - subtração(-)");
+            System.out.println("\t\t[2] - multiplicação(x)");
+            System.out.println("\t\t[3] - subitração(%)");
+            System.out.println("\t\t[4] - sair");
+            System.out.println("-----------------------------------------------------");
+            System.out.print("\tDigite a operação desejada: ");
+            operacao = scan.nextInt();
+            switch (operacao){
+                case 0:
+                    System.out.println("SOMA");
+                    break;
+                case 1:
+                    System.out.println("SUBTRAÇÃO");
+                    break;
+                case 2:
+                    System.out.println("MULTIPLICAÇÃO");
+                    break;
+                case 3:
+                    System.out.println("DIVISÃO");
+                    break;
+                case 4:
+                    System.out.println("SAINDO ...");
+                    break;
+                default:
+                    System.out.println("numero invalido, digite um numero entre 0 e 4 ");
+                    break;
+            }
+        }*/
+        
+        
+        do {
+            System.out.println("-----------------------MENU--------------------------");
+            System.out.println("\t\t[0] - soma(+)");
+            System.out.println("\t\t[1] - subtração(-)");
+            System.out.println("\t\t[2] - multiplicação(x)");
+            System.out.println("\t\t[3] - Divisão(/)");
+            System.out.println("\t\t[4] - sair");
+            System.out.println("-----------------------------------------------------");
+            System.out.print("\tDigite a operação desejada: ");
+            operacao = scan.nextInt();
+            int num1 = 0;
+            int num2 = 0;
+            
+            switch (operacao){
+                case 0:
+                    System.out.print("Digite o primeiro numero da soma: ");
+                    num1 = scan.nextInt();
+                    System.out.print("Digite o segundo numero da soma: ");
+                    num2 = scan.nextInt();
+                    System.out.println( num1 + num2);
+                    break;
+                case 1:
+                    System.out.print("Digite o primeiro numero da subtração: ");
+                    num1 = scan.nextInt();
+                    System.out.print("Digite o segundo numero da subtração: ");
+                    num2 = scan.nextInt();
+                    System.out.println( num1 - num2);
+                    break;
+                case 2:
+                    System.out.print("Digite o primeiro numero da multiplicação: ");
+                    num1 = scan.nextInt();
+                    System.out.print("Digite o segundo numero da multiplicação: ");
+                    num2 = scan.nextInt();
+                    System.out.println( num1 * num2);
+                    break;
+                case 3:
+                    System.out.print("Digite o primeiro numero da divisão: ");
+                    num1 = scan.nextInt();
+                    System.out.print("Digite o segundo numero da divisão: ");
+                    num2 = scan.nextInt();
+                    System.out.println( num1 / num2);
+                    break;
+                case 4:
+                    System.out.println("SAINDO ...");
+                    break;
+                default:
+                    System.out.println("numero invalido, digite um numero entre 0 e 4 ");
+                    break;
+            }
+        }
+        
+        while(operacao != 4);
+            
+     scan.close();
+   }
+}
